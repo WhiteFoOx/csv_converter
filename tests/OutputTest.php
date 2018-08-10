7c67a6176fd4bd4681653796facef80d8d707b40
pavel <pavelgordeev96@gmail.com>
2018-08-10 14:41:51 +0700

    Added testing output files

---
+++ tests/OutputTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+
+final class OutputTest extends TestCase
+{
+    private $filePath;
+
+    protected function setUp()
+    {
+        $this->filePath = '../App/converter.php';
+    }
+
+    /**
+     * @dataProvider additionProvider
+     */
+    public function testOutputNumbers($conf)
+    {
+        $fp = $this->filePath;
+        $input = "files/goodCommaCSV.csv";
+        $output = "files/OutputCSV.csv";
+        require_once '../App/checkoutFile.php';
+        exec("php " . $fp . " -i $input -c $conf -o $output", $output_res, $return_res);
+        if (checkoutFile($input) && checkoutFile($output)) {
+            $handle = fopen($input, "r");
+                $data_input = fgetcsv($handle);
+                fclose($handle);
+            $handle2 = fopen($output, "r");
+                $data_output = fgetcsv($handle2);
+                fclose($handle2);
+        }
+        $this->assertFalse(is_numeric($data_output[0]));
+        $this->assertTrue($data_output[1] == $data_input[1]);
+        $this->assertFalse(is_numeric($data_output[2]));
+        $this->assertTrue(is_numeric($data_output[3]));
+    }
+
+    public function additionProvider()
+    {
+        return [
+            ["files/OutputConf.php"],
+        ];
+    }
+}
---
+++ tests/files/OutputConf.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    0 => function ($value, $rowData, $rowIndex, $faker) {
+        return "string value";
+    },
+];
